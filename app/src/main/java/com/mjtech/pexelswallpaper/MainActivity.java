23adc54655f2fa2f8bd4eb4b3304064ce569b7c4
Muhsanjaved048 <67718185+Muhsanjaved048@users.noreply.github.com>
2021-03-15 16:57:13 +0500

    Initial commit

---
+++ app/src/main/java/com/mjtech/pexelswallpaper/MainActivity.java
@@ -0,0 +1,134 @@
+package com.mjtech.pexelswallpaper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.AbsListView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    WallpaperAdapter wallpaperAdapter;
+    List<WallpaperModel> wallpaperModelList;
+    int pageNumber = 1;
+    Boolean isScrolling  = false;
+    int currentItems,totalItems,scrollOutItems;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        wallpaperModelList = new ArrayList<>();
+        wallpaperAdapter = new WallpaperAdapter(this,wallpaperModelList);
+
+        recyclerView.setAdapter(wallpaperAdapter);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                    isScrolling = true;
+                }
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                currentItems = gridLayoutManager.getChildCount();
+                totalItems = gridLayoutManager.getItemCount();
+                scrollOutItems = gridLayoutManager.findFirstCompletelyVisibleItemPosition();
+
+                if (isScrolling && (currentItems+scrollOutItems == totalItems)){
+                    isScrolling = false;
+                    fetchWallpaper();
+                }
+
+
+            }
+        });
+        fetchWallpaper();
+
+    }
+
+    public void fetchWallpaper(){
+        StringRequest request = new StringRequest(Request.Method.GET, "https://api.pexels.com/v1/curated/?page="+pageNumber+"&per_page=80",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            JSONArray jsonArray =  jsonObject.getJSONArray("photos");
+
+                            int length = jsonArray.length();
+
+                            for (int i =0; i<length;i++){
+
+                                JSONObject object = jsonArray.getJSONObject(i);
+
+                                int id = object.getInt("id");
+
+                                JSONObject objectImages = object.getJSONObject("src");
+
+                                String originalUrl = objectImages.getString("original");
+                                String mediumUrl = objectImages.getString("medium");
+
+                                WallpaperModel wallpaperModel = new WallpaperModel(id,originalUrl,mediumUrl);
+                                wallpaperModelList.add(wallpaperModel);
+                            }
+
+                            wallpaperAdapter.notifyDataSetChanged();
+                            pageNumber++;
+                        }catch (JSONException e){
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+//                params.put("Authorization","563492ad6f917000010000016190f711658e4eb1a96ff1213e707d57");
+                params.put("Authorization","563492ad6f917000010000016190f711658e4eb1a96ff1213e707d57");
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(request);
+    }
+}
